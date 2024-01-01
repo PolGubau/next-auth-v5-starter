509915894c756be2cfb84a5a34bc1f69e89ca393
Pol Gubau <gubaupol@gmail.com>
2024-01-01 23:27:39 +0100

    Fix email sender in mail.tsx

--- lib/mail.ts
+++ lib/mail.ts
@@ -4,9 +4,11 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const domain = process.env.NEXT_PUBLIC_APP_URL;
 
+const sender = "onboarding@resend.dev";
+
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: "gubaupol@gmail.com",
+    from: sender,
     to: email,
     subject: "2FA Code",
     html: `<p>Your 2FA code: ${token}</p>`,
@@ -17,7 +19,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: "gubaupol@gmail.com",
+    from: sender,
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
@@ -28,7 +30,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: "gubaupol@gmail.com",
+    from: sender,
     to: email,
     subject: "Confirm your email",
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
